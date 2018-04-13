e29ca495c66bfd85f7191c5c703440de3ab5785e
xosrika <gkhos15@freeuni.edu.ge>
2018-04-13 22:36:50 +0400

    Added skeleton code

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+  <head>
+	<style>
+		.group:after {
+		  content: "";
+		  display: table;
+		  clear: both;
+		}
+
+		#logo{
+			float: left;
+		}	
+			
+	</style>
+    <meta charset="utf-8">
+		<meta name="author" content="Giorgi Khosroshvili">
+		<meta name="description" content="This is the home page of facebook">
+    <title>Facebook</title>
+  </head>
+  <body>
+	<div id="navbar" class="group">
+		<form id="foom" action="/index.php">
+			<img id="logo" src="logo.png" height="48" width="48">
+		</form>
+	</div> <!-- #navbar -->
+
+	<div id="left-column" class="group">
+
+	</div> <!-- #left-column -->
+
+	<div id="middle-column" class="group">
+
+	</div> <!-- #middle-column -->
+
+	<div id="right-column" class="group">
+
+	</div> <!-- #right-column -->
+	
+	<div id="friends-column" class="group">
+
+	</div> <!-- #friends-column -->
+
+  </body>
+		
+
+</html>
