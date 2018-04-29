84acb81376f60ad0f3a796db58adb8c6265a53d7
xosrika <gkhos15@freeuni.edu.ge>
2018-04-29 14:16:51 +0400

    Made some small changes in CSS

--- index.php
+++ index.php
@@ -11,26 +11,26 @@
   <body>
 	<div id="navbar" class="group">
 		<form id="left-bar" action="index.php">
-			<img id="logo" src="logo1.png">
+			<img id="logo" src="images\logo1.png">
 			<div class="group search-elems" >
 				<input type="text" id="search" name="search" placeholder="Search Facebook">
-				<button id="search-button"> <img  src="search.png" id="search-button-img"> </button>
+				<button id="search-button"> <img  src="images\search.png" id="search-button-img"> </button>
 			</div>
 		</form> <!-- #left-bar -->
 		<div id="right-bar">		
-			<img id="bar-dropdown" src="dropdownheader.png">
-			<img id="help" src="help.png">
+			<img id="bar-dropdown" src="images\dropdownheader.png">
+			<img id="help" src="images\help.png">
 			<span class="navbar-line"></span>
-			<img id="globe" src="globe.png">
-			<img id="messages" src="messenger.png">
-			<img id="friend-req" src="friendrequest.png">
+			<img id="globe" src="images\globe.png">
+			<img id="messages" src="images\messenger.png">
+			<img id="friend-req" src="images\friendrequest.png">
 			
 			<a href="index.php">Fiend Friends</a>
 			<span class="navbar-line"></span>
 			<a href="index.php">Home</a>
 			<span class="navbar-line"></span>	
 			<a href="index.php" >Giorgi</a>			
-			<img id="person" src="person.jpg">
+			<img id="person" src="images\person.jpg">
 			
 		</div> <!-- #right-bar -->
 
@@ -42,21 +42,21 @@
 
 			<a href="#">
 				<div id="user-content" class="group" >
-					<img class="left-column-picture" src="person.jpg">
+					<img class="left-column-picture" src="images\person.jpg">
 					<span class="left" >Giorgi Khosroshvili</span>
 				</div> <!-- #user column -->
 			</a>
 
 			<a href="#">
 				<div id="news-feed" class="group">
-					<img class="left-column-picture" src="news-feed.jpeg">
+					<img class="left-column-picture" src="images\news-feed.jpeg">
 					<span class="left-list-elem left"><b>News Feed</b></span>
 				</div> <!-- #news feed -->
 			</a>
 
 			<a href="#">
 				<div id="left-messages" class="group">
-					<img class="left-column-picture" src="messages-left.jpg">
+					<img class="left-column-picture" src="images\messages-left.jpg">
 					<span class="left-list-elem left">Messages</span>
 				</div><!-- #left messages -->
 			</a>
@@ -68,7 +68,7 @@
 				<a href="#">				
 					<li class="left-list-li">
 						<div>
-							<img class="left-column-picture" src="group.png"> 
+							<img class="left-column-picture" src="images\group.png"> 
 							<span class="left-list-elem">OS group</span>
 						</div>
 					</li>
@@ -77,7 +77,7 @@
 				<a href="#">
 					<li class="left-list-li">
 						<div>
-							<img class="left-column-picture" src="group.png">
+							<img class="left-column-picture" src="images\group.png">
 							<span class="left-list-elem">Frontend Stuff</span>
 						</div>
 					</li>
@@ -86,7 +86,7 @@
 				<a href="#">
 					<li class="left-list-li">
 						<div>
-							<img class="left-column-picture" src="group.png"> 
+							<img class="left-column-picture" src="images\group.png"> 
 							<span class="left-list-elem">Korkot Mishvele</span>
 						</div>
 					</li>
@@ -100,7 +100,7 @@
 				<a href="#">
 					<li class="left-list-li">
 						<div>
-							<img class="left-column-picture" src="pages.jpg"> 
+							<img class="left-column-picture" src="images\pages.jpg"> 
 							<span class="left-list-elem">Pages</span>
 						</div>
 					</li>
@@ -110,7 +110,7 @@
 				<a href="#">
 					<li class="left-list-li">
 						<div>
-							<img class="left-column-picture" src="group.png">				 
+							<img class="left-column-picture" src="images\group.png">				 
 							<span class="left-list-elem">Groups</span>
 						</div>
 					</li>
@@ -120,7 +120,7 @@
 				<a href="#">
 					<li class="left-list-li">
 						<div>
-							<img class="left-column-picture" src="events.png"> 
+							<img class="left-column-picture" src="images\events.png"> 
 							<span class="left-list-elem">Events</span>
 						</div>
 					</li>
@@ -130,7 +130,7 @@
 				<a href="#">
 					<li class="left-list-li">
 						<div>
-							<img class="left-column-picture" src="friends-list.jpg"> 
+							<img class="left-column-picture" src="images\friends-list.jpg"> 
 							<span class="left-list-elem">Friends List</span>	
 						</div>		
 					</li>
@@ -139,7 +139,7 @@
 				<a href="#">
 					<li class="left-list-li">
 						<div>
-							<img class="left-column-picture" src="dropdown.png"> 
+							<img class="left-column-picture" src="images\dropdown.png"> 
 							<span class="left-list-elem left-column-more-color">See More...</span>
 						</div>
 					</li>
@@ -167,7 +167,7 @@
 				<ul class="group">	
 					<a href="#">				
 						<li class="form-header form-header-active">
-							<img class="middle-column-picture" src="create-page.png"> 
+							<img class="middle-column-picture" src="images\create-page.png"> 
 							<b class="middle-list-elem">Create a Post</b>
 						</li>
 					</a>	
@@ -178,7 +178,7 @@
 
 					<a href="#">
 						<li class="form-header">
-							<img class="middle-column-picture" src="album.png"> 
+							<img class="middle-column-picture" src="images\album.png"> 
 							<b class="middle-list-elem">Photo/Video Album</b>
 						</li>
 					</a>
@@ -188,14 +188,14 @@
 
 					<a href="#">
 						<li class="form-header">
-							<img class="middle-column-picture" src="live.png"> 
+							<img class="middle-column-picture" src="images\live.png"> 
 							<b class="middle-list-elem">Live Video</b>
 						</li>
 					</a>		
 				</ul>	
 
 				<div id="main-form" class="group">
-					<img id="form-img" src="person.jpg">
+					<img id="form-img" src="images\person.jpg">
 
 					<textarea placeholder="What's on your mind?"></textarea>				
 
@@ -203,14 +203,14 @@
 					
 					<a href="#">
 						<span class="form-bottom group">
-							<img class="middle-column-picture" src="camera.png"> 
+							<img class="middle-column-picture" src="images\camera.png"> 
 							<span class="middle-list-elem" ><b>Photo/Video</b></span>
 						</span>
 					</a>	
 
 					<a href="#">
 						<span class="form-bottom group">
-							<img class="middle-column-picture" src="smile.png"> 
+							<img class="middle-column-picture" src="images\smile.png"> 
 							<span class="middle-list-elem" ><b>Emotion</b></span>
 						</span>
 					</a>	
@@ -224,7 +224,7 @@
 			<div id="middle-content"> 
 				<div class="post group">
 					<div class="group post-header post-elem">
-						<img class="post-person-img" src="person.jpg">
+						<img class="post-person-img" src="images\person.jpg">
 
 						<p class="post-title-paragraph">
 							<span class="post-author-name">Someone</span> 
@@ -236,7 +236,7 @@
 					</div>
 
 					<div class="post-elem">	
-						<img class="post-photo" src="bird.jpg">
+						<img class="post-photo" src="images\bird.jpg">
 					</div>
 					<div class="post-elem post-footer group">
 						
@@ -251,12 +251,15 @@
 
 					<div class="comments group post-elem">
 						<div class="comments-header group">
+							<img class='comments-header-img left' src="images\like.png">
 							<span class="left">7 Likes</span>
+							
 							<span class="right">Seen by 20</span>
+							<img class='comments-header-img right' src="images\checked.png">
 						</div>
 						<hr>
 						<div class="comment group">
-							<img class="comment-person-image" src="person.jpg">
+							<img class="comment-person-image" src="images\person.jpg">
 							<span class="comment-text group"> 
 								<span class="comment-text-author">Someone</span>	
 								I Love this bird <3
@@ -271,7 +274,7 @@
 						</div><!--comment-->
 
 						<div class="comment group">
-							<img class="comment-person-image" src="popkha.jpg">
+							<img class="comment-person-image" src="images\popkha.jpg">
 							<span class="comment-text group"> 
 								<span class="comment-text-author">Irakli Popkhadze</span>	
 								რა ჩიტია, რა ჩიტი.
@@ -285,7 +288,7 @@
 							</div>
 						</div><!--comment-->
 						<div class="comment group">
-							<img class="comment-person-image" src="vache.jpg">
+							<img class="comment-person-image" src="images\vache.jpg">
 							<span class="comment-text group"> 
 								<span class="comment-text-author">Vache Katsadze</span>	
 								ჩიტო გვრიტო, მარგალიტო.
@@ -299,7 +302,7 @@
 							</div>
 						</div><!--comment-->
 						<div class="comment group">
-							<img class="comment-person-image" src="cero.jpg">
+							<img class="comment-person-image" src="images\cero.jpg">
 							<span class="comment-text group"> 
 								<span class="comment-text-author">Giorgi Cercvadze</span>	
 								რა ჩიტი აგიტყდათ ამხელა ხალხს.
@@ -314,9 +317,13 @@
 						</div><!--comment-->
 
 						<div class="comment group">
-							<img class="comment-person-image" src="person.jpg">
+							<img class="comment-person-image" src="images\person.jpg">
 							<span class="comment-write group"> 
-								<input type="text" class="comment-write-input" name="friend-search" placeholder="Write a comment">
+								<input type="text" class="comment-write-input" name="friend-search" placeholder="Write a comment...">
+								
+								<img class='comment-write-img right' src="images\gif.png">
+								<img class='comment-write-img right' src="images\photo.png">
+								<img class='comment-write-img right' src="images\emoji.png">
 							</span>
 							
 						</div><!--comment-->
@@ -325,7 +332,7 @@
 				</div> <!-- .post -->
 				<div class="post group">
 					<div class="group post-header post-elem" >
-						<img class="post-person-img" src="mark.jpg">
+						<img class="post-person-img" src="images\mark.jpg">
 
 						<p class="post-title-paragraph">
 							<span class="post-author-name">Mark Zuckerberg</span> 
@@ -355,7 +362,7 @@
 
 				<div class="post group">
 					<div class="group post-header post-elem">
-						<img class="post-person-img" src="jeffbezos.jpg">
+						<img class="post-person-img" src="images\jeffbezos.jpg">
 
 						<p class="post-title-paragraph">
 							<span class="post-author-name">Jeff Bezos</span> 
@@ -369,7 +376,7 @@
 					<div class="post-elem">
 						<a href="https://www.amazon.com/" >
 							<div class="post-link group">
-								<img class="post-photo" src="amazon.jpg">
+								<img class="post-photo" src="images\amazon.jpg">
 								
 								<div >
 									<h2 class="post-link-title">Amazon!</h2>
@@ -404,27 +411,33 @@
 			
 			<div class="right-div group">
 				<p class="right-list-elem"><b>STORIES</b></p>
-
+				<a href="#"> 
+					<p class="right-list-elem right"> <b>
+						<span class="right-list-dots"> </span>
+						<span class="right-list-dots"> </span>
+						<span class="right-list-dots"> </span>
+					</b></p>
+				</a>
 				<div class="story group" >
-					<img class="story-picture" src="vache.jpg">
+					<img class="story-picture" src="images\vache.jpg">
 					<p class="story-author">Vache Katsadze</p>
 					<p class="story-time" >10 hours ago</p>
 				</div>
 
 				<div class="story group">
-					<img class="story-picture" src="cero.jpg">
+					<img class="story-picture" src="images\cero.jpg">
 					<p class="story-author">Giorgi Cercvadze</p>
 					<p class="story-time" >1 hour ago</p>
 				</div>
 
 				<div class="story group">
-					<img class="story-picture"  src="popkha.jpg">
+					<img class="story-picture"  src="images\popkha.jpg">
 					<p class="story-author">Irakli Popkhadze</p>
 					<p class="story-time">4 hours ago</p>
 				</div>
 
 				<div class="group">
-					<img class="right-column-dropdown left" src="dropdown.png" >
+					<img class="right-column-dropdown left" src="images\dropdown.png" >
 					<p class="see-more">See more</p>
 				</div>
 			</div>	<!--.right-div group-->
@@ -432,20 +445,20 @@
 			<div class="right-div group">
 
 				<p class="right-list-elem">YOUR PAGES</p>
-				<img class="right-column-dropdown" src="dropdown.png"> 
+				<img class="right-column-dropdown" src="images\dropdown.png"> 
 
 				<hr>
 				<div>
-					<img class="right-column-buffer" src="buffer.png">
+					<img class="right-column-buffer" src="images\buffer.png">
 					<button id="buffer-button">Buffer</button> 
 					<p class="right-column-buffer-text">20+ Messages</p>
-					<img class="right-column-dropdown-buffer" src="dropdown.png">
+					<img class="right-column-dropdown-buffer" src="images\dropdown.png">
 				</div><!-- your pages -->
 				<hr>
 
 				<p class="right-list-elem" >Create an Event for Your Page</p>
 				<div class="group alone-line">
-					<img class="event-picture"  src="calendar.png">
+					<img class="event-picture"  src="images\calendar.png">
 					<p class="right-list-sub-elem" > 
 					Make it easy for people to find your event. Create it on Facebook.</p>
 					<button id="event-button">Cerate Event</button> 
@@ -457,7 +470,7 @@
 				<p class="right-list-elem right">8 hours ago</p>
 
 				<div  class="group alone-line">
-					<img class="event-picture" src="joe.png">
+					<img class="event-picture" src="images\joe.png">
 					<p class="right-list-sub-elem-name"> 
 					Joe Estes</p>
 					<p class="right-list-sub-elem" > 
@@ -482,7 +495,7 @@
 				<div id="recent-posts">
 					<div class="group alone-line" >
 						<div  class="group alone-line">
-							<img class="left-post-logo" src="recent-post.jpg">
+							<img class="left-post-logo" src="images\recent-post.jpg">
 							<a href="#">
 									<p class="right-list-sub-elem right-list-post-link">
 										A Nobel prize-winning physicist identified...
@@ -494,7 +507,7 @@
 
 					<div class="group alone-line">
 						<div  class="group alone-line">
-							<img class="left-post-logo" src="recent-post.jpg">
+							<img class="left-post-logo" src="images\recent-post.jpg">
 							<a href="#">
 								<p class="right-list-sub-elem right-list-post-link">
 									Buffer co-founfers Joel &amp; Leo enjoing...
@@ -520,11 +533,11 @@
 							more
 						</span>	
 						<div class="friends-games-list group">
-							<img class="friends-game-arrow" src="arrow-left.png">
-							<img class="friends-game-img" src="flash-game-4.jpg">
-							<img class="friends-game-img" src="flash-game-5.jpg">
-							<img class="friends-game-img" src="flash-game-6.jpg">
-							<img class="friends-game-arrow" src="arrow-right.png">	
+							<img class="friends-game-arrow" src="images\arrow-left.png">
+							<img class="friends-game-img" src="images\flash-game-4.jpg">
+							<img class="friends-game-img" src="images\flash-game-5.jpg">
+							<img class="friends-game-img" src="images\flash-game-6.jpg">
+							<img class="friends-game-arrow" src="images\arrow-right.png">	
 						</div><!-- #friend-games-list -->
 					</div>		
 					<hr>
@@ -536,34 +549,34 @@
 							more
 						</span>	
 						<div class="friends-games-list group">
-							<img class="friends-game-arrow" src="arrow-left.png">
-							<img class="friends-game-img" src="flash-game-1.jpg">
-							<img class="friends-game-img" src="flash-game-2.jpg">
-							<img class="friends-game-img" src="flash-game-3.jpg">
-							<img class="friends-game-arrow" src="arrow-right.png">	
+							<img class="friends-game-arrow" src="images\arrow-left.png">
+							<img class="friends-game-img" src="images\flash-game-1.jpg">
+							<img class="friends-game-img" src="images\flash-game-2.jpg">
+							<img class="friends-game-img" src="images\flash-game-3.jpg">
+							<img class="friends-game-arrow" src="images\arrow-right.png">	
 						</div><!-- #friend-games-list -->
 					</div> 		
 				</div> <!-- #friend-games -->
 				
 				<div id="friend-list">
 					<div class="friend-list-elem group">
-						<img class="friend-list-elem-img" src="joe.png">
+						<img class="friend-list-elem-img" src="images\joe.png">
 						<span class="friend-list-elem-name">Joe Estes</span>
 						<span class="friend-list-elem-time">1 min</span>
 					</div>	
 					<div class="friend-list-elem group">
-						<img class="friend-list-elem-img" src="cero.jpg">
+						<img class="friend-list-elem-img" src="images\cero.jpg">
 						<span class="friend-list-elem-name">Giorgi Cercvadze</span>
 						<span class="friend-list-elem-online"> </span>
 					</div>	
 
 					<div class="friend-list-elem group">
-						<img class="friend-list-elem-img" src="popkha.jpg">
+						<img class="friend-list-elem-img" src="images\popkha.jpg">
 						<span class="friend-list-elem-name">Irakli Popkhadze</span>
 						<span class="friend-list-elem-online"> </span>
 					</div>	
 					<div class="friend-list-elem group">
-						<img class="friend-list-elem-img" src="vache.jpg">
+						<img class="friend-list-elem-img" src="images\vache.jpg">
 						<span class="friend-list-elem-name">Vache Katsadze</span>
 						<span class="friend-list-elem-time">30 min</span>
 					</div>	
@@ -578,14 +591,14 @@
 			<div id="friend-search" class="group">
 				
 				<button class="search-friend-button-glass"> 
-					<img class="search-button" src="search.png"> 
+					<img class="search-button" src="images\search.png"> 
 				</button>
 				
 				
 				<input type="text" id="friend-search-area" name="friend-search" placeholder="Search">
 
 				<button class="search-friend-button-cog" > 
-					<img class="search-button right"  src="cog.png"> 
+					<img class="search-button right"  src="images\cog.png"> 
 				</button>				
 				
 			</div><!-- #friend-search -->
